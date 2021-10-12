adbd7e9d9cb3ef831bb8b27e8015d1b77f43dd80
DILLON CRAW <68667382+crawnic311@users.noreply.github.com>
2021-10-12 13:49:50 -0600

    Part 2

--- server/index.js
+++ server/index.js
@@ -7,6 +7,14 @@ app.use(express.json())
 app.use(cors())
 
 
+const controller = require('./controller')
+
+app.get('api/houses/', controller.getHouses)
+app.get('api/houses/', controller.createHouse)
+app.get('api/houses/:id', controller.updateHouse)
+app.get('api/houses/:id', controller.deleteHouse)
+
+
 
 
 app.listen(4004, () => console.log('Unity 4004'))

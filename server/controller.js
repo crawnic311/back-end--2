8856baddc41ba808977f5e5aa032a9434e4fc1e3
DILLON CRAW <68667382+crawnic311@users.noreply.github.com>
2021-10-12 13:40:48 -0600

    Part 1

---
+++ server/controller.js
@@ -0,0 +1,20 @@
+const houses = require('./db.json')
+
+
+
+
+
+module.exports = {
+    getHouses: (req, res) => {
+
+    },
+    deleteHouse: (req, res) => {
+        
+    },
+    createHouse: (req, res) => {
+        
+    },
+    updateHouse: (req, res) => {
+        
+    }
+}
